dde2b3695b50ca590d4a2b7aac51620702025b88
muhammeta7 <muhammeta7@gmail.com>
2021-01-20 20:45:56 -0500

    Strings section with examples for syntax with quotes, variables, & user input.

---
+++ CourseMaterial/Fundamentals/Strings/strings.py
@@ -0,0 +1,16 @@
+# Strings can be surround by single or double quotes.
+# NOTE: To make things simpler, start and end with the same quotations.
+print("Yay, I am learning strings in Python.")
+print('Works the same with single quotes')
+print("Python's strings are very simple to use.")
+print('You can do "quotes" within strings.')
+
+# Concatenation is when you add multiple strings together
+# Store strings into variables so that concatenation makes sense
+greeting = "Hello"
+name = "Moe"
+print(greeting + ' ' + name) # Hello Moe
+
+# Example with input from the user by assigning the variable to input function, and then printing after.
+userInput = input("Please enter your name ")
+print(greeting + ' ' + userInput) # Hello [Word typed in from user]
--- README.md
+++ README.md
@@ -43,7 +43,7 @@ This repo is to document my knowledge and practice of **Python**. Below is a lis
     1. Hello World and a few other combinations for print function.
         * [Hello World](https://github.com/muhammeta7/Python-Tutorial/blob/main/CourseMaterial/Fundamentals/HelloWorld/helloworld.py) 
     2. Strings      
-
+        * 
         
         
         
