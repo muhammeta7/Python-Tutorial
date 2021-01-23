a614e78432abe01bb1b70bec9cbf76c14e3c0874
muhammeta7 <muhammeta7@gmail.com>
2021-01-23 14:57:21 -0500

    Code examples of operator precedence.

--- CourseMaterial/Fundamentals/Operators/operators.py
+++ CourseMaterial/Fundamentals/Operators/operators.py
@@ -10,18 +10,16 @@ print(a // b)    # 4
 print(a % b)    # 0
 print(a ** b)    # 1728
 
-# Shorthand
-for i in range(1, a//b):
-    print(i) # i is expression because it needs to be evaluated
-# Expressions 1 evaluates to 1, a//b evaluates to 4, range(1, a//b) needs to be evaluated to get 4.
-# Can't have an expression bound to a value on the left of an assignment
+# -------------------------------------------------------------------------
 
-# LongHand
-i = 1
-print(i)
-i = 2
-print(i)
-i = 3
-print(i)
-
+# Operator Precedence / Order of Operations
+print(a + b / 3 - 4 * 12)   # -35.0
+print(a + (b/3) - (4 * 12))  # 35.0
+print((((a + b) / 3) - 4) * 12)  # 12.0
+print(((a + b) / 3 - 4) * 12)
+#  Do not be afraid to break it up into smaller parts
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
 
